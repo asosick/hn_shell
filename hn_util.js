448b84e0b2b5214087cf76f4c36b6567e48528f7
August Sosick <august.sosick@ucalgary.ca>
2022-02-14 22:22:19 -0700

    Moved js files to src folder

--- hn_util.js
+++
@@ -1,9 +0,0 @@
-import { fetchItem } from "./hn_api.js"
-
-export async function getNItems(top_stories){
-    let tenLinks = []
-    for(const id of top_stories){
-        tenLinks.push(await fetchItem(id))
-    }
-    return tenLinks
-}

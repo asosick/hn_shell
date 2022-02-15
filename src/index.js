688ef51103a27c488b450d2249294775e060521f
August Sosick <august.sosick@ucalgary.ca>
2022-02-14 22:22:45 -0700

    Moved js files to src folder

---
+++ src/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import figlet from 'figlet';
+import { fetchTopStories, fetchItem } from "./hn_api.js";
+import { displayStories } from './hn_display.js';
+import ora from 'ora';
+
+
+console.clear()
+
+const choice = await displayStories()
+console.log(choice)
